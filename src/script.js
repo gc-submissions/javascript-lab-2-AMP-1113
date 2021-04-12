58dc8f44d907e5cb6237b0aa23e68c19f710595f
adam <paddenadam@gmail.com>
2021-04-12 16:59:54 -0400

    final submission including extended challenges

--- src/script.js
+++ src/script.js
@@ -13,7 +13,7 @@ const attackPlayer = function(health) {
 
 const logHealth = (player, health) => console.log(`${player} health: ${health}`);
 const logDeath = (winner, loser) => console.log(`${winner} defeated ${loser}`);
-const isDead = health => health <=0 ? true : false;
+const isDead = health => health <= 0 ? true : false;
 
 function fight(player1, player2, player1Health, player2Health) {
     while (true) {
@@ -38,3 +38,65 @@ function fight(player1, player2, player1Health, player2Health) {
 
 fight("Adam", "Zee", 100, 100);
 
+// EXTENDED CHALLENGES
+
+//Print Sqaure Function
+function printSquare(width) {
+  let count = 1;
+  while (count <= width) {
+      count++;
+      console.log("###");
+  }
+}
+printSquare(3);
+
+// Print Triangle Function
+function printTriangle(width) {
+    let count = 1
+    let start = "";
+    while (count <= width) {
+        start += "#";
+        count++;
+        console.log(start)
+    } 
+  }
+  printTriangle(10);
+
+
+  /* GET GRADE FUNCTION */
+  function getGrade(percentageGrade) {
+    if (percentageGrade >= 90) {
+        return "A";
+    } else if (percentageGrade >= 80) {
+        return "B";
+    } else if (percentageGrade >= 70) {
+        return "C";
+    } else if (percentageGrade >= 60) {
+        return "D";
+    } else {
+        return "F";
+    }
+  }
+  console.log(getGrade(80));
+  console.log(getGrade(77));
+  console.log(getGrade(94));
+  console.log(getGrade(66));
+
+  // Prioritize Function
+
+  const prioritize = (urgent, important) => {
+      if (urgent && important) {
+          return 1
+      } else if (important && !urgent) {
+          return 2
+      } else if (urgent && !important) {
+          return 3
+      } else {
+          return 4
+      }
+  }
+
+  console.log(prioritize(true, true));
+  console.log(prioritize(false, true));
+  console.log(prioritize(true, false));
+  console.log(prioritize(false, false));
